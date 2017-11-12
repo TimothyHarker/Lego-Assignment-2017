05933d64325b861a850fe24887efa82368334a98
Myles-Glenn Gaerlan <m.gaerlan@live.com>
2017-11-13 08:55:36 +1100

    Ground Zero

---
+++ LegoClasses/Legobot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Desktop;
+using Lego.Ev3.Core;
+
+namespace LegoClasses
+{
+    public class Legobot
+    {
+        public Brick brick { get; set; }
+
+        public async void ConnectToBrick()
+        {
+            brick = new Brick(new UsbCommunication());
+            await brick.ConnectAsync();
+        }
+
+    }
+}
---
+++ LegoConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LegoClasses;
+
+namespace LegoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Legobot et = new Legobot();
+        }
+    }
+}
