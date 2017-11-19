5fc9db883e35e9de64cd5b4fb96cc4dac5cb4340
hamishrlyall <hamishrlyall@gmail.com>
2017-11-20 09:43:01 +1100

    Read Colour Turn

--- LegoClasses/Legobot.cs
+++ LegoClasses/Legobot.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Lego.Ev3.Desktop;
 using Lego.Ev3.Core;
+using System.Threading;
 
 namespace LegoClasses
 {
@@ -16,11 +17,12 @@ namespace LegoClasses
         {
             brick = new Brick(new UsbCommunication());
             await brick.ConnectAsync();
+            brick.Ports[InputPort.Two].SetMode(ColorMode.Color);
         }
 
         public async void Forward()
         {
-            await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.A | OutputPort.D, 30);
+            await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.A | OutputPort.D, 30, 1000, false);
             //brick.BatchCommand.TurnMotorAtPower(OutputPort.A | OutputPort.D, 30);
             //await brick.BatchCommand.SendCommandAsync();
         }
@@ -35,17 +37,24 @@ namespace LegoClasses
             await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.A | OutputPort.D, 0, 2000, false);
         }
 
-        public async void Turn15Left()
+        public async void Turn15Right()
         {
             brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 30, 300, false);
             brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -30, 300, false);
             await brick.BatchCommand.SendCommandAsync();
         }
 
-        public async void Turn15Right()
+        public async void Turn15Left()
         {
             brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, -30, 300, false);
             brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, 30, 300, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        public async void Turn90Right()
+        {
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 30, 1000, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -30, 1000, false);
             await brick.BatchCommand.SendCommandAsync();
         }
 
@@ -56,11 +65,47 @@ namespace LegoClasses
             await brick.BatchCommand.SendCommandAsync();
         }
 
-        public async void Turn90Right()
+        public float DetectDistance()
         {
-            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 30, 1000, false);
-            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -30, 1000, false);
-            await brick.BatchCommand.SendCommandAsync();
+            return brick.Ports[InputPort.Three].SIValue;
+        }
+
+        public float DetectColour()
+        {
+            float[] Colours = new float[10];
+            int i = 0;
+            while (i < 10)
+            {
+                Colours[i] = Colours[i] + brick.Ports[InputPort.Two].SIValue;
+                i++;
+            }
+
+            Colours.Sum();
+            Colours.Average();
+            float ColourDetected = Colours.Average();
+            return ColourDetected;
+        }
+
+        public async void ReadColourTurn()
+        {
+            float distance = brick.Ports[InputPort.Three].SIValue;
+            if (distance < 6)
+            {
+                DetectColour();
+                Console.WriteLine(DetectColour());
+                if (DetectColour() == 0)
+                {
+                    Stop();
+                    Reverse();
+                    Turn15Right();
+                }
+                else
+                {
+                    Stop();
+                    Reverse();
+                    Turn15Left();
+                }
+            }
         }
 
     }
--- LegoConsole/Program.cs
+++ LegoConsole/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LegoClasses;
-
+using Lego.Ev3.Desktop;
+using Lego.Ev3.Core;
+using System.Threading;
 namespace LegoConsole
 {
     class Program
@@ -13,9 +15,35 @@ namespace LegoConsole
         {
             Legobot et = new Legobot();
             et.ConnectToBrick();
+            Thread.Sleep(2000);
+            float distance = et.DetectDistance();
+            Console.WriteLine("Distance Detected: "+ distance);
+            while (distance > 6)
+            { 
+                et.Forward();
+                Thread.Sleep(2000);
+                distance = et.DetectDistance();
+                Console.WriteLine("Distance Detected: "+ distance);
+            }
+            et.DetectColour();
+            float ColourDetected = et.DetectColour();
+            Console.WriteLine("Colour Detected: "+ ColourDetected);
+            if (ColourDetected == 0)
+            {
+                Thread.Sleep(2000);
+                et.Reverse();
+                Thread.Sleep(2000);
+                et.Turn15Right();
+            }
+            else
+            {
+                Thread.Sleep(2000);
+                et.Reverse();
+                Thread.Sleep(2000);
+                et.Turn15Left();
 
-            et.Turn90Left();
 
+            }
             Console.ReadLine();
         }
     }
